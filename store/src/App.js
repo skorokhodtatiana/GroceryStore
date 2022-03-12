01e8ddefb0b6731ff6aa224f044bc6c2def1bcec
tskorohod <tskorohod@lmteam.by>
2022-03-12 20:01:11 +0300

    started project

---
+++ store/src/App.js
@@ -0,0 +1,12 @@
+import './assets/App.scss';
+import Header from'./components/header/Header.jsx'
+
+function App() {
+	return (
+		<div className="App">
+			<Header></Header>
+		</div>
+	);
+}
+
+export default App;
---
+++ store/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import './_header.scss';
+
+const Header = () => {
+	return (
+		<div className='headerWrapper'>
+			<h1 className='headerWrapper__title'>GroceryStoreBY</h1>
+			<nav>
+				<a href='mmm'>Обо мне</a>
+			</nav>
+		</div>
+	)
+}
+
+export default Header;
