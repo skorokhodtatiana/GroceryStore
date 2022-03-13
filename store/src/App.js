48f312ee8e86f2e225c93f0b62f29361c2cc518e
tskorohod <tskorohod@lmteam.by>
2022-03-13 22:36:58 +0300

    started to create menu-nav, main page

--- store/src/App.js
+++ store/src/App.js
@@ -1,11 +1,34 @@
 import './assets/App.scss';
-import Header from'./components/header/Header.jsx'
+import Header from'./components/header/Header.jsx';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link,
+	BrowserRouter
+} from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<Header></Header>
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<Header></Header>
+				<Routes>
+					<Route exact path="/"></Route>
+					<Route exact path="catalog"></Route>
+					<Route exact path="payment"></Route>
+					<Route exact path="reviews"></Route>
+					<Route path="*"
+						element={
+							<main style={{ padding: "1rem", textAlign: "center", fontSize: "3rem" }}>
+								<h1>404</h1>
+								<h3>Not found</h3>
+							</main>
+						}
+					/>
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 
--- store/src/components/header/Header.jsx
+++ store/src/components/header/Header.jsx
@@ -1,13 +1,51 @@
 import './_header.scss';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+  } from "react-router-dom";
+import mainphoto from '../../assets/images/photo-main.jpg';
 
 const Header = () => {
 	return (
-		<div className='headerWrapper'>
-			<h1 className='headerWrapper__title'>GroceryStoreBY</h1>
-			<nav>
-				<a href='mmm'>Обо мне</a>
-			</nav>
-		</div>
+		<>
+			<div className="headerWrapper">
+				<h1 className="headerWrapper__title">GroceryStoreBY</h1>
+				<nav className="headerWrapper__nav">
+					<ul className="headerWrapper__list">
+						<li className="headerWrapper__list-item">
+							<Link to="/" className="link">
+								Обо мне
+							</Link>
+						</li>
+						<li className="headerWrapper__list-item">
+							<Link to="/catalog" className="link">
+								Каталог
+							</Link>
+						</li>
+						<li className="headerWrapper__list-item">
+							<Link to="/payment" className="link">
+								Оплата и доставка
+							</Link>
+						</li>
+						<li className="headerWrapper__list-item">
+							<Link to="reviews" className="link">
+								Отзывы
+							</Link>
+						</li>
+						<li className="headerWrapper__list-item">
+							<Link to="games" className="link">
+								Игры
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div className="wrapper-img">
+				<img className="wrapper-img__img-main" src={ mainphoto } alt={ "mainphoto" }></img>
+			</div>
+		</>
 	)
 }
 
