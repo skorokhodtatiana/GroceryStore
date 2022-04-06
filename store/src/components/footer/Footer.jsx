cb700cf67c915ac046dc0166baf1eade2e8f61b5
tskorohod <tskorohod@lmteam.by>
2022-04-06 22:13:18 +0300

    added picture, footer in process

--- store/src/components/footer/Footer.jsx
+++ store/src/components/footer/Footer.jsx
@@ -2,15 +2,34 @@ import './_footer.scss';
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faFacebook, faVk} from "@fortawesome/free-brands-svg-icons";
-import fisch from '../../assets/images/fisch.jpg';
+import apple from '../../assets/images/apple-removebg.png';
+import pumpkin from '../../assets/images/pumpkin-removebg.png';
+import iceCream from '../../assets/images/iceCream-removebg.png';
+import mushroom from '../../assets/images/mushroom-removebg.png';
+import radish from '../../assets/images/radish-removebg.png';
+
+
 
 const Footer = () => {
-	return (<>
-	<div className='footer__picture-wrapper'>
-				<img className='footer__picture' src={fisch} alt={"fisch"}></img>
-			</div>
+	return (
 		<div className='footer'>
-			
+			<div className='footer__gallery'>
+				<div className='footer__picture-wrapper'>
+					<img className='footer__picture' src={apple} alt={"apple"}></img>
+				</div>
+				<div className='footer__picture-wrapper'>
+					<img className='footer__picture' src={pumpkin} alt={"pumpkin"}></img>
+				</div>
+				<div className='footer__picture-wrapper'>
+					<img className='footer__picture' src={iceCream} alt={"iceCream"}></img>
+				</div>
+				<div className='footer__picture-wrapper'>
+					<img className='footer__picture' src={mushroom} alt={"mushroom"}></img>
+				</div>
+				<div className='footer__picture-wrapper'>
+					<img className='footer__picture' src={radish} alt={"radish"}></img>
+				</div>
+			</div>
 			<div className='footer__content'>
 				<a href='https://www.instagram.com/natashina_pekarnya/'>
 					<FontAwesomeIcon icon={faInstagram} className={"footer__icon iconInstagram"}/>
@@ -24,7 +43,6 @@ const Footer = () => {
 			</div>
 			<div className='footer__author'>© 2022 Natalia Tatarginskaja</div>
 		</div>
-		</>
 	)
 };
 
