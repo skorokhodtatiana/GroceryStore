ce097568dd2b04a3ff4e90966023fb65c9212d0f
tskorohod <tskorohod@lmteam.by>
2022-06-19 23:10:20 +0300

    in process showing reviews

--- store/src/components/reviews/Rewiews.jsx
+++ store/src/components/reviews/Rewiews.jsx
@@ -3,17 +3,20 @@ import { useState } from "react";
 const Reviews = () => {
 	const [review, setReview] = useState();
 
+	const createNewReview = (e) => {
+		setReview(e.target.value);
+	}
+
 	const showRevievs = () => {
 		const reviewBlock = document.getElementById('reviewBlock');
-		
-			reviewBlock.innerHTML = "иииии";
+		reviewBlock.innerHTML = review;
 		
 	}
 	return(
 		<div className="review">
 			<div className="review__new">
 				<label for="textarea" className="review__leave">Оставте свой отзыв</label>
-				<textarea value={review} type="text" placeholder="Напишите здесь все, чем хотите поделиться" id="textarea" className="review__textarea"></textarea>
+				<textarea value={review} type="text" onChange={(e) => createNewReview(e)} placeholder="Напишите здесь все, чем хотите поделиться" id="textarea" className="review__textarea"></textarea>
 				<button onClick={showRevievs} className="review__button">Поделиться</button>
 			</div>
 			<div class="review__block" id="reviewBlock">
