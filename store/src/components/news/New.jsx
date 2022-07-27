5ca550c0314e0ffbf2532d473e0b9acea1ee98cf
tskorohod <tskorohod@lmteam.by>
2022-07-27 15:10:56 +0300

    fixed bag

--- store/src/components/news/New.jsx
+++ store/src/components/news/New.jsx
@@ -1,8 +1,11 @@
+import iceCream from '../../assets/images/iceCream.jpg';
+import './news.scss';
+
 const New = () => {
 	return(
 		<>
 			<h2>В последнее время на создание новинок меня толкает моя маленькая крестница, большая любительница мороженного...</h2>
-			<img src="../assets/images/iceCream.jpg"></img>
+			<img className="new-icecream" src={iceCream}></img>
 		</>
 	)
 }
