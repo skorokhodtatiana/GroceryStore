8799086e1ce933c03df8b5dd7ae7394969edeb54
tskorohod <tskorohod@lmteam.by>
2022-07-28 12:00:36 +0300

    added new photos

--- store/src/components/news/New.jsx
+++ store/src/components/news/New.jsx
@@ -1,11 +1,19 @@
 import iceCream from '../../assets/images/iceCream.jpg';
+import food from '../../assets/images/food.jpg';
+import sweets from '../../assets/images/sweets.jpg';
+
 import './news.scss';
 
 const New = () => {
 	return(
 		<>
 			<h2>В последнее время на создание новинок меня толкает моя маленькая крестница, большая любительница мороженного...</h2>
-			<img className="new-icecream" src={iceCream}></img>
+			<div>
+				<img className="new-icecream" src={iceCream}></img>
+				<img className="new-icecream" src={food}></img>
+				<img className="new-icecream" src={sweets}></img>
+			</div>
+			
 		</>
 	)
 }
