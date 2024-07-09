3e5b98fe2c5a3cffe002505ebbb68a4d2ff76353
tskorohod <tskorohod@lmteam.by>
2024-07-09 23:15:09 +0300

    start doing preview

--- store/src/components/catalogue/Catalogue.jsx
+++ store/src/components/catalogue/Catalogue.jsx
@@ -1,5 +1,25 @@
 import { DataCards } from '/Users/tatsianaskarakhod/Documents/github/GroceryStore/store/src/assets/DataCards';
 
+const CardItemPreviev = (props) => {
+	const {title, image, description, price, discount} = props;
+	return(
+		<div>
+			<div className='card'>
+				{/* <h2 className='card__title'>{title}</h2> */}
+				<div className='card__wrapper-img'>
+					<img className='card__img' src={image} alt={title}></img>
+				</div>
+				<div className='card__description-block'>
+				<div className='card__price-block'>
+					<div className='card__price'>{price}</div>
+					<div className='card__discount'>{discount}</div>
+				</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
 const CardItem = (props) => {
 	const {title, image, description, price} = props;
 	return(
