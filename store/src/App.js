de8d35f158e9f2cec1c03e5cb2565ed645b400b1
tskorohod <tskorohod@lmteam.by>
2024-08-02 17:16:28 +0300

    changing styles

--- store/src/App.js
+++ store/src/App.js
@@ -21,22 +21,24 @@ function App() {
 		<BrowserRouter>
 			<div className="app">
 				<Header></Header>
-				<Routes>
-					<Route exact path="/" element={<MainBlock/>}></Route>
-					<Route exact path="aboutMe" element={<AboutMe/>}></Route>
-					<Route exact path="catalogue" element={<ListItem/>}></Route>
-					<Route exact path="payment" element={<Payment/>}></Route>
-					<Route exact path="reviews" element={<Reviews/>}></Route>
-					<Route exact path="new" element={<New/>}></Route>
-					<Route path="*"
-						element={
-							<main style={{ padding: "1rem", textAlign: "center", fontSize: "3rem" }}>
-								<h1>404</h1>
-								<h3>Not found</h3>
-							</main>
-						}
-					/>
-				</Routes>
+				<div className="main-app-block">
+					<Routes>
+						<Route exact path="/" element={<MainBlock/>}></Route>
+						<Route exact path="aboutMe" element={<AboutMe/>}></Route>
+						<Route exact path="catalogue" element={<ListItem/>}></Route>
+						<Route exact path="payment" element={<Payment/>}></Route>
+						<Route exact path="reviews" element={<Reviews/>}></Route>
+						<Route exact path="new" element={<New/>}></Route>
+						<Route path="*"
+							element={
+								<main style={{ padding: "1rem", textAlign: "center", fontSize: "3rem" }}>
+									<h1>404</h1>
+									<h3>Not found</h3>
+								</main>
+							}
+						/>
+					</Routes>
+				</div>
 				<Footer></Footer>
 			</div>
 		</BrowserRouter>
--- store/src/components/header/Header.jsx
+++ store/src/components/header/Header.jsx
@@ -3,39 +3,39 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<>
-			<div className="headerWrapper">
-				<h1 className="headerWrapper__title">
+		<div className="headerWrapper">
+			<div className="header">
+				<h1 className="header__title">
 					<Link to="/" className="link-title">GroceryStoreBY</Link>
 				</h1>
-				<nav className="headerWrapper__nav">
-					<ul className="headerWrapper__list">
-						<li className="headerWrapper__list-item headerWrapper__about-me">
+				<nav className="header__nav">
+					<ul className="header__list">
+						<li className="header__list-item header__about-me">
 							<Link to="/aboutMe" className="link">
 								Обо мне
 							</Link>
 						</li>
-						<li className="headerWrapper__list-item">
+						<li className="header__list-item">
 							<Link to="/catalogue" className="link">
 								Каталог
 							</Link>
 						</li>
-						<li className="headerWrapper__list-item">
+						<li className="header__list-item">
 							<Link to="/payment" className="link">
 								Оплата и доставка
 							</Link>
 						</li>
-						<li className="headerWrapper__list-item">
+						<li className="header__list-item">
 							<Link to="reviews" className="link">
 								Отзывы
 							</Link>
 						</li>
-						<li className="headerWrapper__list-item">
+						<li className="header__list-item">
 							<Link to="games" className="link">
 								Игры
 							</Link>
 						</li>
-						<li className="headerWrapper__list-item">
+						<li className="header__list-item">
 							<Link to="new" className="link">
 								Новинки
 							</Link>
@@ -43,7 +43,7 @@ const Header = () => {
 					</ul>
 				</nav>
 			</div>
-		</>
+		</div>
 	)
 }
 
